58a24fdc00186d8df02b1c06720d0788ae1c6467
Matěj Gordon <achjon338@gmail.com>
2024-03-21 12:37:36 +0100

    Added timeout when the node is not avalible

--- src/ha_resource_status_exporter.py
+++ src/ha_resource_status_exporter.py
@@ -6,6 +6,7 @@ import yaml
 from prometheus_client import start_http_server, Gauge
 import time
 import logging
+import socket
 
 # Set logging level
 logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(message)s')
@@ -18,34 +19,47 @@ def parse_arguments():
     return parser.parse_args()
 
 # Fuction to authenticate and get token
-def authenticate(proxmox_node, proxmox_user, proxmox_password, verify_ssl):
+def authenticate(proxmox_node, proxmox_user, proxmox_password, verify_ssl, proxmox_node_port):
     """Authenticate and get token."""
-    auth_url = f"https://{proxmox_node}:8006/api2/json/access/ticket"
+    auth_url = f"https://{proxmox_node}:{proxmox_node_port}/api2/json/access/ticket"
     auth_data = {
         "username": proxmox_user,
         "password": proxmox_password
     }
-    try:
-        response = requests.post(auth_url, data=auth_data, verify=verify_ssl)
-        auth_response = response.json()
-    except requests.exceptions.SSLError as e:
-        logging.error(f"SSL verification failed: {e}")
-        if verify_ssl:
-            logging.warning("SSL verification is enabled. Please check the certificate. If it's self-signed, you can disable SSL verification by setting verify_ssl to false in the config file.")
+    def check_ip_availability(ip, port):
+        """Check if the IP is available on the specified port."""
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(10)  # Set a timeout of 2 seconds
+        result = sock.connect_ex((ip, port))
+        sock.close()
+        return result == 0
+
+    # Check if the IP is available before making the request
+    if check_ip_availability(proxmox_node, proxmox_node_port):
+        try:
+            response = requests.post(auth_url, data=auth_data, verify=verify_ssl)
+            auth_response = response.json()
+        except requests.exceptions.SSLError as e:
+            logging.error(f"SSL verification failed: {e}")
+            if verify_ssl:
+                logging.warning("SSL verification is enabled. Please check the certificate. If it's self-signed, you can disable SSL verification by setting verify_ssl to false in the config file.")
+            sys.exit(errno.EACCES)
+        if str(auth_response) == "{'data': None}":
+            logging.error("Authentication to the PVE unsuccessful")
+            sys.exit(errno.EACCES)
+        # Extract ticket and CSRFPreventionToken
+        ticket = auth_response['data']['ticket']
+        csrf_token = auth_response['data']['CSRFPreventionToken']
+        cookies = {
+            'PVEAuthCookie': ticket
+        }
+        headers = {
+            'CSRFPreventionToken': csrf_token
+        }
+        return cookies, headers
+    else:
+        logging.error(f"Proxmox node {proxmox_node} is not available on port {proxmox_node_port}. Please check the IP and port.")
         sys.exit(errno.EACCES)
-    if str(auth_response) == "{'data': None}":
-        logging.error("Authentication to the PVE unsuccessful")
-        sys.exit(errno.EACCES)
-    # Extract ticket and CSRFPreventionToken
-    ticket = auth_response['data']['ticket']
-    csrf_token = auth_response['data']['CSRFPreventionToken']
-    cookies = {
-        'PVEAuthCookie': ticket
-    }
-    headers = {
-        'CSRFPreventionToken': csrf_token
-    }
-    return cookies, headers
 
 VM_STATE = Gauge('vm_state', 'State of the VM', ['id', 'node', 'group', 'status', 'state'])
 def process_vm_state(vm_state):
@@ -90,6 +104,7 @@ def main():
     proxmox_password = config["default"]["password"]
     verify_ssl = config["default"].get("verify_ssl", True)
     scrape_interval = config["default"].get("scrape_interval", 60)
+    proxmox_node_port = config["default"].get("proxmox_node_port", 8006)
 
     if not isinstance(proxmox_node, str):
         sys.exit("proxmox_node must be a string")
@@ -101,7 +116,6 @@ def main():
         sys.exit("verify_ssl must be a boolean")
     if not isinstance(scrape_interval, int):
         sys.exit("scrape_interval must be an integer")
-
     if not isinstance(verify_ssl, bool):
         sys.exit("verify_ssl must be a boolean")
 
@@ -112,8 +126,8 @@ def main():
     start_http_server(args.port)
     logging.info(f"Server started on http://0.0.0.0:{args.port}")
 
-    cookies, headers = authenticate(proxmox_node, proxmox_user, proxmox_password, verify_ssl)
-    data_url = f"https://{proxmox_node}:8006/api2/json/cluster/ha/status/current"
+    cookies, headers = authenticate(proxmox_node, proxmox_user, proxmox_password, verify_ssl, proxmox_node_port)
+    data_url = f"https://{proxmox_node}:{proxmox_node_port}/api2/json/cluster/ha/status/current"
 
     while True:
         try:
